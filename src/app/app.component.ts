9f61061045afeb8358e9bf996991870662c1e559
Kevin Alexis Rodríguez Condori <sis414.kevinrodriguez17@gmail.com>
2024-05-11 12:18:16 -0400

    nav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import {RouterLink, RouterOutlet} from '@angular/router';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { log } from 'node:console';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +11,36 @@ import {RouterLink, RouterOutlet} from '@angular/router';
 })
 export class AppComponent {
   title = 'seguro-universitario';
+  @ViewChild('nav') nav!: ElementRef;
+  @ViewChild('institucional') institucional!: ElementRef;
+  @ViewChild('servicios') servicios!: ElementRef;
+  @ViewChild('personalSalud') personalSalud!: ElementRef;
+  @ViewChild('contrataciones') contrataciones!: ElementRef;
+  hadleClick() {
+    this.nav.nativeElement.classList.toggle('active');
+  }
+
+  removeClassTogle() {
+    this.nav.nativeElement.classList.toggle('active');
+  }
+
+  closeMenu(target: string) {
+    switch (target) {
+      case 'institucional':
+        this.institucional.nativeElement.classList.toggle('hidden');
+
+        break;
+      case 'servicios':
+        this.servicios.nativeElement.classList.toggle('hidden');
+        break;
+      case 'personalSalud':
+        this.personalSalud.nativeElement.classList.toggle('hidden');
+        break;
+      case 'contrataciones':
+        this.contrataciones.nativeElement.classList.toggle('hidden');
+        break;
+      default:
+        break;
+    }
+  }
 }
